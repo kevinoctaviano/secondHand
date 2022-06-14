b7d6e49cbd220336efb520807b5b7c630dafb3e3
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-06-14 15:51:46 +0700

    push login and register

---
+++ frontend/src/components/pages/components/SidePicture.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import sidePic from '../../assets/svg/side-picture.svg';
+
+export default function SidePicture() {
+  return <img className="w-100" src={sidePic} alt="SecondHand SidePicture" />;
+}
