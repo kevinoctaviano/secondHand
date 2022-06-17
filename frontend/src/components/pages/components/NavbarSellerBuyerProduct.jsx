6bfc420e6a4145365ba63b07caedb3f1edd7b060
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-06-17 19:31:45 +0700

    push navbar dropdown and dashboard daftar jual

--- frontend/src/components/pages/components/NavbarSellerBuyerProduct.jsx
+++ frontend/src/components/pages/components/NavbarSellerBuyerProduct.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Navbar, Container, Nav, Form } from 'react-bootstrap';
+import { Navbar, Container, Nav, Form, NavDropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import fi_bell from '../../assets/svg/fi_bell.svg';
 import fi_user from '../../assets/svg/fi_user.svg';
 import fi_list from '../../assets/svg/fi_list.svg';
+import jam from '../../assets/svg/jam.svg';
 
 export default function NavbarSellerBuyerProduct() {
   return (
@@ -32,9 +33,64 @@ export default function NavbarSellerBuyerProduct() {
             <Nav.Link href="#action1">
               <img src={fi_list} alt="List" />
             </Nav.Link>
-            <Nav.Link href="#action1">
-              <img src={fi_bell} alt="Bell" />
-            </Nav.Link>
+            <NavDropdown
+              title={<img src={fi_bell} alt="Bell" />}
+              id="basic-nav-dropdown"
+              align="end"
+            >
+              <NavDropdown.Item href="#">
+                <div className="row">
+                  <div className="col-lg-2">
+                    <img
+                      src={jam}
+                      alt="Jam"
+                      style={{
+                        width: '48px',
+                        height: '48px',
+                        borderRadius: '12px',
+                      }}
+                    />
+                  </div>
+                  <div className="col-lg-7">
+                    <p className="text-muted m-0 label-10px">
+                      Penawaran Produk
+                    </p>
+                    <h6>Jam Tangan Casio</h6>
+                    <h6>Rp 250.000</h6>
+                    <h6>Ditawar Rp 250.000</h6>
+                  </div>
+                  <div className="col-lg-3">
+                    <p className="text-muted m-0 label-10px">20 Apr, 14:04</p>
+                  </div>
+                </div>
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">
+                <div className="row">
+                  <div className="col-lg-2">
+                    <img
+                      src={jam}
+                      alt="Jam"
+                      style={{
+                        width: '48px',
+                        height: '48px',
+                        borderRadius: '12px',
+                      }}
+                    />
+                  </div>
+                  <div className="col-lg-7">
+                    <p className="text-muted m-0 label-10px">
+                      Berhasil diterbitkan
+                    </p>
+                    <h6>Jam Tangan Casio</h6>
+                    <h6>Rp 250.000</h6>
+                  </div>
+                  <div className="col-lg-3">
+                    <p className="text-muted m-0 label-10px">19 Apr, 12:00</p>
+                  </div>
+                </div>
+              </NavDropdown.Item>
+            </NavDropdown>
             <Nav.Link href="#action1">
               <img src={fi_user} alt="User" />
             </Nav.Link>
