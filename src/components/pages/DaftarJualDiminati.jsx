5d8f41235bc7b718382a647e481b8d4f7cca01e6
Kevin Octaviano <76242149+kevinoctaviano@users.noreply.github.com>
2022-07-18 06:44:23 +0700

    add style, get wishlist user

--- src/components/pages/DaftarJualDiminati.jsx
+++ src/components/pages/DaftarJualDiminati.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import kosong from '../assets/svg/kosong.svg';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import Sidebar from './components/Sidebar';
 import Topbar from './components/Topbar';
+
+import { getAllWishlist } from '../../actions/user';
 
 const mapStateToProps = (state) => {
   return {
     // user
     isNullUser: state.user.isNull,
     user: state.user.user,
+    wishlist: state.wishlist.wishlist,
   };
 };
 
-const DaftarJualDiminati = () => {
+const DaftarJualDiminati = (props) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllWishlist);
+  }, [dispatch]);
+  // Mengubah format currency menjadi format rupiah
+  let formatter = new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+  });
   // const user = JSON.parse(localStorage.getItem('user'));
   return (
     <div className="container mt-4">
@@ -21,11 +33,42 @@ const DaftarJualDiminati = () => {
         <Topbar />
         <div className="row">
           <Sidebar />
-          <div className="col-md-8 d-flex justify-content-center">
+          <div className="col-md-8">
             <div className="row">
-              <div className="col-md-4">
-                <img src={kosong} alt="Kosong" />
-              </div>
+              {props.wishlist.length !== 0 ? (
+                props.wishlist.map((item) => (
+                  <div
+                    className="col-lg-4 d-flex justify-content-center"
+                    key={item.product.idProduct}
+                  >
+                    <div className="card card-daftar-jual mb-3 shadow-md px-2 pt-2 pb-4">
+                      <div className="d-flex justify-content-center">
+                        <img
+                          src={item.product.imageProduct[0]?.urlImage}
+                          className="card-home"
+                          style={{ height: '100px' }}
+                          alt={item.product.namaProduct}
+                        />
+                      </div>
+                      <h5 className="mt-1 text-sm font-normal">
+                        {item.product.namaProduct}
+                      </h5>
+                      <h5 className="mt-1 text-10px font-normal text-muted">
+                        {item.product.kategori.namaKategori}
+                      </h5>
+                      <h5 className="mt-1 text-sm font-normal">
+                        {formatter.format(item.product.hargaProduct)}
+                      </h5>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div className="d-flex justify-content-center">
+                  <div className="col-lg-4">
+                    <img src={kosong} alt="Kosong" />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
